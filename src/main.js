baaa68b606a75cc93cbfdf9abeef6ff1e4126fe7
Vadzim.Nikolsky <nikolskivadim@gmail.com>
2019-08-28 16:59:47 +0300

    feat: added table with the ability to search and sort

--- src/main.js
+++ src/main.js
@@ -5,11 +5,13 @@ import store from './store';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-import { LayoutPlugin, NavPlugin, ListGroupPlugin } from 'bootstrap-vue';
+import { LayoutPlugin, NavPlugin, ListGroupPlugin, TablePlugin, FormInputPlugin } from 'bootstrap-vue';
 
 Vue.use(LayoutPlugin);
 Vue.use(NavPlugin);
 Vue.use(ListGroupPlugin);
+Vue.use(TablePlugin);
+Vue.use(FormInputPlugin);
 
 Vue.config.productionTip = false;
 
--- src/views/warehousesList/components/list/index.vue
+++ src/views/warehousesList/components/list/index.vue
@@ -5,12 +5,68 @@
       <b-list-group-item>NIKE</b-list-group-item>
       <b-list-group-item>AMAZON.COM</b-list-group-item>
     </b-list-group>
+    <div>
+      <b-form-input placeholder="search" type="text" v-model="keyword"></b-form-input>
+      <b-table head-variant="light" hover :items="filterItems" :fields="fields" :keyword="keyword"></b-table>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'warehousesList'
+    name: 'warehousesList',
+    data () {
+      return {
+        fields: [
+          {
+            key: 'company_name',
+            sortable: true
+          },
+          {
+            key: 'address',
+            sortable: true
+          },
+          {
+            key: 'description',
+            sortable: false
+          },
+          {
+            key: 'date',
+            sortable: true
+          }
+        ],
+        items: [
+          {
+            company_name: 'IKEA',
+            address: 'USA',
+            description: 'Swedish-founded multinational group that designs and sells ready-to-assemble furniture',
+            date: new Date(1943, 6, 28)
+          },
+          {
+            company_name: 'NIKE',
+            address: 'Washington County, Oregon, U.S. (Near Beaverton, Oregon)',
+            description: ' American multinational corporation that is engaged in ' +
+              'the design, development, manufacturing, and worldwide marketing ' +
+              'and sales of footwear, apparel, equipment, accessories, and services.',
+            date: new Date(1964, 0, 25)
+          },
+          {
+            company_name: 'AMAZON.COM',
+            address: 'Seattle, Washington, and Arlington, Virginia, United States',
+            description: 'American multinational technology company based in Seattle, Washington',
+            date: new Date(1994, 6, 5)
+          }
+        ],
+        keyword: ''
+      };
+    },
+    computed: {
+      filterItems() {
+        return this.keyword ? this.items.filter(item =>
+          item.company_name.includes(this.keyword) ||
+          item.address.includes(this.keyword)) : this.items;
+      }
+    }
   };
 </script>
 
