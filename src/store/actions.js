27ec54abe85d06f83214c3e12846a50a15e54b3a
Oigen43 <oigen43@icloud.com>
2019-08-31 20:20:04 +0300

    feat: add mutation-types

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,6 +1,7 @@
 import api from '../utils/api';
+import * as mutationTypes from './mutation-types';
 
 export const commitWarehousesList = async({ commit }) => {
     let req = await api.fetchData();
-    commit('COMPANIES', req);
+    commit(mutationTypes.COMPANIES, req);
 };
