438201ee929ea5cce6791c60266ab7cff9b4ddfd
vikkichild <vikkichild137@gmail.com>
2019-09-04 11:36:00 +0300

    feat: added go back link

--- src/views/WCompaniesAddForm/components/WForm/index.vue
+++ src/views/WCompaniesAddForm/components/WForm/index.vue
@@ -1,32 +1,32 @@
 <template>
   <div>
     <b-form @submit="onSubmit" v-if="show" class="w-companies-add-form">
-        <b-form-input
-          id="company-name-input"
-          size="lg"
-          v-model="form.name"
-          required
-          placeholder="Company name"
-          class="w-companies-add-input"
-        ></b-form-input>
-
-        <b-form-input
-          id="company-address-input"
-          size="lg"
-          v-model="form.address"
-          required
-          placeholder="Company address"
-          class="w-companies-add-input"
+      <b-form-input
+        id="company-name-input"
+        size="lg"
+        v-model="form.name"
+        required
+        placeholder="Company name"
+        class="w-companies-add-input"
       ></b-form-input>
 
-        <b-form-input
-          id="company-description-input"
-          size="lg"
-          v-model="form.description"
-          required
-          placeholder="Company description"
-          class="w-companies-add-input"
-        ></b-form-input>
+      <b-form-input
+        id="company-address-input"
+        size="lg"
+        v-model="form.address"
+        required
+        placeholder="Company address"
+        class="w-companies-add-input"
+      ></b-form-input>
+
+      <b-form-input
+        id="company-description-input"
+        size="lg"
+        v-model="form.description"
+        required
+        placeholder="Company description"
+        class="w-companies-add-input"
+      ></b-form-input>
 
       <b-button
         type="submit"
@@ -36,7 +36,14 @@
       >
         ADD COMPANY
       </b-button>
+
     </b-form>
+    <b-button
+      variant="link"
+      to="/companies"
+      class="w-companies-add-link"
+    >Go Back
+    </b-button>
   </div>
 </template>
 
