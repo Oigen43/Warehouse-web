b76a0784ae1d6319d56578fc46c980148ab2c9aa
vikkichild <vikkichild137@gmail.com>
2019-09-26 14:03:02 +0300

    fix: fixed responsive design for cards

--- src/views/WUsersList/components/WList/index.vue
+++ src/views/WUsersList/components/WList/index.vue
@@ -1,49 +1,45 @@
 <template>
-    <b-row>
-      <b-col lg="4" md="6" sm="12" v-for="user in users" v-bind:key="user.id">
+    <div class="w-users-cards-container">
+      <div class="w-users-card-wrap" v-for="user in users" v-bind:key="user.id">
         <b-card no-body class="overflow-hidden w-users-card">
-          <b-row no-gutters>
-            <b-col lg="6" md="12">
-              <div class="w-role-label">Admin</div>
-              <b-card-img src="https://www.zayedhotel.com/addons/default/themes/yoona/img/user.jpg" class="rounded-0"
-                           alt="User image"></b-card-img>
-            </b-col>
-            <b-col lg="6" md="12">
-              <b-card-body class="w-users-card-body">
-                <b-card-title class="mb-0">{{user.firstName}} {{user.patronymic}} {{user.surname}}</b-card-title>
-                <hr>
-                <b-card-text><span class="w-users-card-text">Date of Birth:</span> {{user.birthDate}}</b-card-text>
-                <b-card-text><span class="w-users-card-text">Email:</span> {{user.email}}</b-card-text>
-                <b-card-text><span class="w-users-card-text">Address:</span> {{user.address}}</b-card-text>
-              </b-card-body>
-              <b-card-footer>
-                <b-button
-                  class="w-list-button"
-                  variant="warning"
-                  size="sm"
-                  @click="clickedUpdateButton(user)">
-                  Update
-                </b-button>
-                <b-button
-                  class="w-list-button"
-                  variant="outline-dark"
-                  size="sm"
-                  @click="clickedDeleteButton(user)">
-                  ✕
-                </b-button>
-              </b-card-footer>
-            </b-col>
-          </b-row>
+          <div class="w-users-card-role-label">Admin</div>
+          <b-card-img src="https://www.zayedhotel.com/addons/default/themes/yoona/img/user.jpg"
+                      class="rounded-0 w-users-card-img"
+                      alt="User image"></b-card-img>
+          <b-card-body class="w-users-card-body">
+            <b-card-title class="mb-0">{{user.firstName}} {{user.patronymic}} {{user.surname}}</b-card-title>
+            <hr>
+            <b-card-text><span class="w-users-card-text">Date of Birth:</span> {{user.birthDate}}</b-card-text>
+            <b-card-text><span class="w-users-card-text">Email:</span> {{user.email}}</b-card-text>
+            <b-card-text><span class="w-users-card-text">Address:</span> {{user.address}}</b-card-text>
+          </b-card-body>
+          <b-card-footer class="w-users-card-footer">
+            <b-button
+              class="w-users-card-button"
+              variant="warning"
+              size="sm"
+              @click="clickedUpdateButton(user)">
+              Update
+            </b-button>
+            <b-button
+              class="w-users-card-button"
+              variant="outline-dark"
+              size="sm"
+              @click="clickedDeleteButton(user)">
+              ✕
+            </b-button>
+          </b-card-footer>
         </b-card>
-      </b-col>
-    </b-row>
+      </div>
+    </div>
 </template>
 
 <script>
-    import { mapActions } from 'vuex';
+    import {mapActions} from 'vuex';
     import {
         BRow,
         BCol,
+        BCardGroup,
         BCard,
         BCardImg,
         BCardTitle,
@@ -61,6 +57,7 @@
         components: {
             BRow,
             BCol,
+            BCardGroup,
             BCard,
             BCardImg,
             BCardTitle,
@@ -74,8 +71,8 @@
             return {
                 fields: [
                     'firstName', 'surname', 'patronymic', 'birthDate', 'email', 'address',
-                    { key: 'update', label: '' },
-                    { key: 'delete', label: '' }
+                    {key: 'update', label: ''},
+                    {key: 'delete', label: ''}
                 ]
             };
         },
