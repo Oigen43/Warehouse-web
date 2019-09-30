e2d81ca5c79ff8d9804cd10a7c25431961d8caea
vikkichild <vikkichild137@gmail.com>
2019-09-30 12:19:30 +0300

    resolve: delete unnecessary code

--- src/components/WCompanyForm/index.vue
+++ src/components/WCompanyForm/index.vue
@@ -1,62 +1,62 @@
 <template>
-    <b-form @submit.prevent="onSubmit" class="w-companies-form">
+  <b-form @submit.prevent="onSubmit" class="w-companies-form">
+    <b-form-input
+      id="company-name-input"
+      size="lg"
+      v-model="companyForm.companyName"
+      required
+      placeholder="Company name"
+      class="w-companies-form-input"
+    ></b-form-input>
+
+    <b-form-input
+      id="company-address-input"
+      size="lg"
+      v-model="companyForm.address"
+      required
+      placeholder="Company address"
+      class="w-companies-form-input"
+    ></b-form-input>
+
+    <b-form-input
+      id="company-description-input"
+      size="lg"
+      v-model="companyForm.description"
+      required
+      placeholder="Company description"
+      class="w-companies-form-input"
+    ></b-form-input>
+
+    <div v-if="withAdminFields"
+         class="w-companies-admin"
+    >
+      <h3>Add company admin</h3>
       <b-form-input
-        id="company-name-input"
+        v-model="adminForm.firstName"
         size="lg"
-        v-model="companyForm.companyName"
         required
-        placeholder="Company name"
-        class="w-companies-form-input"
+        placeholder="Admin name"
+        class="w-companies-admin-form-input"
       ></b-form-input>
 
       <b-form-input
-        id="company-address-input"
+        v-model="adminForm.email"
         size="lg"
-        v-model="companyForm.address"
         required
-        placeholder="Company address"
-        class="w-companies-form-input"
+        placeholder="Admin email"
+        class="w-companies-admin-form-input"
       ></b-form-input>
+    </div>
 
-      <b-form-input
-        id="company-description-input"
-        size="lg"
-        v-model="companyForm.description"
-        required
-        placeholder="Company description"
-        class="w-companies-form-input"
-      ></b-form-input>
-
-      <div v-if="withAdminFields"
-        class="w-companies-admin"
-      >
-        <h3>Add company admin</h3>
-        <b-form-input
-          v-model="adminForm.firstName"
-          size="lg"
-          required
-          placeholder="Admin name"
-          class="w-companies-admin-form-input"
-        ></b-form-input>
-
-        <b-form-input
-          v-model="adminForm.email"
-          size="lg"
-          required
-          placeholder="Admin email"
-          class="w-companies-admin-form-input"
-        ></b-form-input>
-      </div>
-
-      <b-button
-        type="submit"
-        variant="outline-success"
-        size="lg"
-        class="w-companies-form-button"
-      >
-        {{ submitButtonName }}
-      </b-button>
-    </b-form>
+    <b-button
+      type="submit"
+      variant="outline-success"
+      size="lg"
+      class="w-companies-form-button"
+    >
+      {{ submitButtonName }}
+    </b-button>
+  </b-form>
 </template>
 
 <script>
@@ -76,7 +76,7 @@
                 type: String,
             },
             id: {
-              type: Number
+                type: Number
             },
             companyName: {
                 type: String,
@@ -119,10 +119,13 @@
         },
         methods: {
             onSubmit() {
-                this.$emit('form-submitted', { company: this.companyForm, user: {
-                    data: this.adminForm,
-                    roles: [userRoles.COMPANY_ADMIN_ROLE]
-                }});
+                this.$emit('form-submitted', {
+                    company: this.companyForm,
+                    user: {
+                        data: this.adminForm,
+                        roles: [userRoles.COMPANY_ADMIN_ROLE]
+                    }
+                });
             }
         }
     };
--- src/components/WTable/index.vue
+++ src/components/WTable/index.vue
@@ -6,7 +6,6 @@
     borderless
     stacked="lg"
     v-bind="$attrs">
-    <slot v-for="slot in Object.keys($slots)" :name="slot" :slot="slot"/>
     <template v-for="slot in Object.keys($scopedSlots)" :slot="slot" slot-scope="scope"><slot :name="slot" v-bind="scope"/></template>
   </b-table>
   </div>
