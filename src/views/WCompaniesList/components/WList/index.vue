bcc9a50fde04b11de0bc8b12e8c030852bd5b39f
vikkichild <vikkichild137@gmail.com>
2019-10-01 18:21:13 +0300

    refactor: cleanup the code

--- src/views/WCompaniesList/components/WList/index.vue
+++ src/views/WCompaniesList/components/WList/index.vue
@@ -58,11 +58,13 @@
             return {
                 fields: [
                     'active', 'companyName', 'address', 'description',
-                    { key: 'date',
-label: 'Date',
-formatter: value => {
-                        return value.slice(0, 10);
-                        } },
+                    {
+                        key: 'date',
+                        label: 'Date',
+                        formatter: value => {
+                            return value.slice(0, 10);
+                        }
+                    },
                     { key: 'warehouses', label: '' },
                     { key: 'buttons', label: '' },
                     { key: 'blank', label: '', class: 'w-blank-column' }
