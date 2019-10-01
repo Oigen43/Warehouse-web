6c6dd6ba54127c780aad06b8d66a49338b945086
Oigen43 <oigen43@icloud.com>
2019-10-01 09:14:13 +0300

    feat: add registration form

--- src/views/WConfirmation/index.vue
+++ src/views/WConfirmation/index.vue
@@ -3,16 +3,50 @@
       <b-col lg="3" sm="12" offset-lg="4">
         <h1 class="w-user-registration-h1">Registration Completion</h1>
         <b-form class="w-users-form">
+
           <b-form-input
             size="lg"
-            placeholder="User first name"
+            v-model="form.surname"
+            placeholder="User surname"
             class="w-users-form-input"
           ></b-form-input>
 
           <b-form-input
             size="lg"
+            v-model="form.patronymic"
+            placeholder="User patronymic"
+            class="w-users-form-input"
+          ></b-form-input>
+
+          <b-form-input
+            size="lg"
+            v-model="form.address"
+            placeholder="User address (city, street, house, flat)"
+            class="w-users-form-input"
+          ></b-form-input>
+
+          <b-form-input
+            size="lg"
+            v-model="form.birthDate"
+            type="date"
+            placeholder="User birth date"
+            class="w-users-form-input"
+          ></b-form-input>
+
+          <b-form-input
+            size="lg"
+            v-model="form.login"
             required
-            placeholder="User surname"
+            placeholder="User login"
+            class="w-users-form-input"
+          ></b-form-input>
+
+          <b-form-input
+            size="lg"
+            v-model="form.password"
+            type="password"
+            required
+            placeholder="User password"
             class="w-users-form-input"
           ></b-form-input>
 
@@ -40,6 +74,18 @@
           BForm,
           BFormInput,
           BButton
+        },
+        data() {
+            return {
+                form: {
+                    surname: '',
+                    patronymic: '',
+                    address: '',
+                    birthDate: '',
+                    login: '',
+                    password: ''
+                },
+            };
         }
     };
 </script>
