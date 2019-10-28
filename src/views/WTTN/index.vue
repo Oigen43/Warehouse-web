c9dd7824b2f67a86a680a83226e24ec158f48a05
vikkichild <vikkichild137@gmail.com>
2019-10-28 17:43:13 +0300

    refactor: fix ttn report style

--- src/views/WTTN/index.vue
+++ src/views/WTTN/index.vue
@@ -40,8 +40,8 @@
 <script>
     import { BRow, BCol, BButton } from 'bootstrap-vue';
     import { mapState, mapActions } from 'vuex';
-    import JsPDF from 'jspdf';
-    import html2canvas from 'html2canvas';
+    import pdfMake from 'pdfmake/build/pdfmake';
+    import pdfFonts from 'pdfmake/build/vfs_fonts';
 
     import types from '../../constants/TTNtypes';
     import WForm from './components/WForm';
@@ -63,7 +63,9 @@
         },
         computed: {
             ...mapState([
-                'updatedTTN'
+                'updatedTTN',
+                'userInfo',
+                'updatedCompany'
             ]),
             registrationDate() {
                 return this.updatedTTN.registrationDate;
@@ -76,29 +78,176 @@
             },
             type() {
                 return types.OUTCOMING_TYPE;
+            },
+            driverFullName() {
+                return `${this.updatedTTN.Driver.firstName} ${this.updatedTTN.Driver.surname}`;
+            },
+            dispatcherFullName() {
+                return `${this.updatedTTN.User.firstName} ${this.updatedTTN.User.surname}`;
             }
         },
         methods: {
             ...mapActions({
-                getUpdatedTTNData: 'getUpdatedTTN'
+                getUpdatedTTNData: 'getUpdatedTTN',
+                fetchUserInfo: 'fetchUserInfo',
+                getUpdatedCompanyData: 'getUpdatedCompany'
             }),
-            clickedDownloadButton() {
-                const pdfName = 'test';
-                const doc = new JsPDF();
-                const element = document.getElementById('content');
+            buildTableBody(data, header, columns) {
+                let body = [];
 
-                html2canvas(element).then(canvas => {
-                    const imgData = canvas.toDataURL('image/jpeg');
-                    doc.addImage(imgData, 'JPEG', 0, -40, 190, 190);
-                    doc.save(pdfName + '.pdf');
+                body.push(header);
+
+                data.forEach(row => {
+                    let dataRow = [];
+
+                    columns.forEach(column => {
+                        dataRow.push(row[column].toString());
+                    });
+
+                    body.push(dataRow);
                 });
+
+                return body;
+            },
+            table(data, header, columns) {
+                return {
+                    style: 'table',
+                    table: {
+                        headerRows: 1,
+                        widths: [100, 50, 80, 50, 70, 115],
+                        body: this.buildTableBody(data, header, columns)
+                    }
+                };
+            },
+            clickedDownloadButton() {
+                pdfMake.vfs = pdfFonts.pdfMake.vfs;
+                const docDefinition = {
+                    content: [
+                        {
+                            text: `Goods Consignment Note #${this.updatedTTN.number}`,
+                            style: 'header',
+                            alignment: 'center'
+                        },
+                        'Company (warehouses owner)',
+                        {
+                            style: 'table',
+                            table: {
+                                widths: [200, 200, 90],
+                                body: [
+                                    ['Name', 'Address', 'Dispatcher'],
+                                    [
+                                        `${this.updatedCompany.companyName}`,
+                                        `${this.updatedCompany.address}`,
+                                        `${this.dispatcherFullName}`
+                                    ]
+                                ]
+                            }
+                        },
+                        'Sender',
+                        {
+                            style: 'table',
+                            table: {
+                                widths: [200, 200, 90],
+                                body: [
+                                    ['Name', 'TIN', 'Country Code'],
+                                    [
+                                        `${this.updatedTTN.Sender ? this.updatedTTN.Sender.senderName : '---'}`,
+                                        `${this.updatedTTN.Sender ? this.updatedTTN.Sender.upn : '---'}`,
+                                        `${this.updatedTTN.Sender ? this.updatedTTN.Sender.countryCode : '---'}`
+                                    ]
+                                ]
+                            }
+                        },
+
+                        'Receiver',
+                        {
+                            style: 'table',
+                            table: {
+                                widths: [200, 200, 90],
+                                height: [200, 200, 90],
+                                body: [
+                                    ['Name', 'TIN', 'Country Code'],
+                                    [
+                                        `${this.updatedTTN.Receiver ? this.updatedTTN.Receiver.receiverName : '---'}`,
+                                        `${this.updatedTTN.Receiver ? this.updatedTTN.Receiver.upn : '---'}`,
+                                        `${this.updatedTTN.Receiver ? this.updatedTTN.Receiver.countryCode : '---'}`
+                                    ]
+                                ]
+                            }
+                        },
+
+                        'Carrier',
+                        {
+                            style: 'table',
+                            table: {
+                                widths: [200, 200, 90],
+                                body: [
+                                    ['Name', 'TIN', 'Country Code'],
+                                    [
+                                        `${this.updatedTTN.Carrier.name}`,
+                                        `${this.updatedTTN.Carrier.upn}`,
+                                        `${this.updatedTTN.Carrier.countryCode}`
+                                    ]
+                                ]
+                            }
+                        },
+
+                        'Transport',
+                        {
+                            style: 'table',
+                            table: {
+                                widths: [250, 250],
+                                body: [
+                                    ['Type', 'Number'],
+                                    [`${this.updatedTTN.Transport.transportType}`, `${this.updatedTTN.Transport.transportNumber}`]
+                                ]
+                            }
+                        },
+
+                        'Driver',
+                        {
+                            style: 'table',
+                            table: {
+                                widths: [164, 164, 164],
+                                body: [
+                                    ['Name', 'Passport Number', 'Licence Issuing Date'],
+                                    [
+                                        `${this.updatedTTN.Driver ? this.driverFullName : '---'}`,
+                                        `${this.updatedTTN.Driver ? this.updatedTTN.Driver.passportNumber : '---'}`,
+                                        `${this.updatedTTN.Driver ? this.updatedTTN.Driver.issuingDate : '---'}`
+                                    ]
+                                ]
+                            }
+                        },
+
+                        'Goods',
+                        this.table(
+                            this.goods,
+                            ['Name', 'Volume', 'Count', 'Weight', 'Price', 'Storage Recommendation'],
+                            ['name', 'volume', 'count', 'weight', 'price', 'recommendation']
+                        )
+                    ],
+                    styles: {
+                        header: {
+                            fontSize: 20,
+                            bold: true,
+                            margin: [0, 0, 0, 20]
+                        },
+                        table: {
+                            margin: [0, 5, 0, 15]
+                        }
+                    }
+                };
+
+                pdfMake.createPdf(docDefinition).open();
             }
         },
         created: async function () {
             await this.getUpdatedTTNData(this.TTNId);
+            await this.fetchUserInfo();
+            await this.getUpdatedCompanyData(this.userInfo.companyId);
         }
-    }
-    ;
+    };
 </script>
 
 <style lang="scss" scoped>
